bda0ae586422159b33fe83766e5d5d3af8abe74f
EReyes13 <itsmeelio13@gmail.com>
2025-05-15 17:15:27 -0400

    enemy fix
    has audio now (Yipeee)

--- Assets/Scripts/enemscript.cs
+++ Assets/Scripts/enemscript.cs
@@ -7,18 +7,20 @@ public class enemscript : MonoBehaviour
     // Update is called once per frame
     public AudioClip Deathsfx;
     public AudioSource audioSource;
+    public float timer = 1;
 
     public void Start()
     {
-        audioSource = gameObject.GetComponent<AudioSource>();
+       // audioSource = gameObject.GetComponent<AudioSource>();
     }
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            audioSource.PlayOneShot(Deathsfx,1);
+            //  audioSource.PlayOneShot(Deathsfx,1);
+            // timer -= Time.deltaTime;
+            Destroy(Instantiate(audioSource, transform.position, Quaternion.identity), audioSource.clip.length);
             Destroy(transform.parent.gameObject);
-
         }
     }
 }
